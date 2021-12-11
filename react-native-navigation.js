c0e8f940800cca02c43bdaf37c5c718d6291f8f8
Themiya Jayakodi <48510475+themiya125@users.noreply.github.com>
2021-12-11 12:07:33 +0530

    Create react-native-navigation.js

---
+++ react-native-navigation.js
@@ -0,0 +1,8 @@
+command - npm install @react-navigation/native
+        -expo install react-native-screens react-native-safe-area-context
+
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
